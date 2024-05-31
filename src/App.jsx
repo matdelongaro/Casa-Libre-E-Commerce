1722682b3d06e0e6172139a4b5641c55260c5017
matias delongaro <matiasadelongaro@gmail.com>
2024-05-31 15:58:42 -0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from  "./components/NavBar.jsx"
+
+function App(){
+  return(
+  <>
+    <NavBar/>
+  </>
+  )
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+function NavBar(){
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Nosotros</li>
+                <li>Ofertas</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default NavBar
