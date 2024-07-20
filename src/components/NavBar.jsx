97136ff6d6214f03e06b9a3cae1b8d754493c97d
matias delongaro <matiasadelongaro@gmail.com>
2024-07-20 17:43:16 -0300

    styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,10 +11,9 @@ function NavBar(){
         <nav>
             <NavLink to="/" className="nav_logo"  onClick={() => { setIsOpen(false);}}>Casa Libre</NavLink>
             <ul className={`nav_items ${isOpen && "open"}`}>
-                <li  onClick={() => { setIsOpen(false);}}><NavLink to="/electrodomesticos">Electrodomésticos</NavLink></li>
+                <li  onClick={() => { setIsOpen(false);}}><NavLink to="/electrodomesticos">Todos los productos</NavLink></li>
                 <li  onClick={() => { setIsOpen(false);}}><NavLink to="/electrodomesticos/cocina">Cocina</NavLink></li>
                 <li  onClick={() => { setIsOpen(false);}}><NavLink to="/electrodomesticos/lavanderia">Lavandería</NavLink></li>
-                <li  onClick={() => { setIsOpen(false);}}><NavLink to="/electrodomesticos/limpieza">Limpieza</NavLink></li>
                 <li  onClick={() => { setIsOpen(false);}}><NavLink to="/electrodomesticos/climatizacion">Climatización</NavLink></li>
                 <li  onClick={() => { setIsOpen(false);}}><NavLink to="/signin">Sign In</NavLink></li>    
                 <li  onClick={() => { setIsOpen(false);}}><NavLink to="/carrito"><CartWidget/></NavLink></li>   
